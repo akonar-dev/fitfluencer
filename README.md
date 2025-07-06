1455531e10a3bb4f5d3e96458896d4ea7c08f3cd
akonar-dev <157737602+akonar-dev@users.noreply.github.com>
2025-07-06 20:37:54 +0530

    Landing Page v1

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 1. Setup Vite
 2. Setup Tailwind CSS
+3. Hero Image and Hero Text(imported Google Font)
+4. Navigation Logo
+5. Basic Navbar with About and Contact Us
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import fitfluencer from "../src/assets/tab-icon.png";
 
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold">
-        This is my fitfluencer application
+      <div className="text-3xl font-bold bg-[url(../src/assets/hero-image.jpg)] backdrop  -opacity-50 bg-cover h-screen">
+        <div className="flex justify-between">
+          <img className="w-1/20 m-10" src={fitfluencer} alt="nav-logo" />
+            <ul className="flex justify-around text-white">
+              <li className="p-5 m-5 text-6xl">About</li>
+              <li className="p-5 m-5 text-6xl">Contact Us</li>
+            </ul>
+        </div>
+        <div className="font-poppins text-9xl text-center text-white tracking-wider relative top-50">
+          Your Fitness Journey Starts Here.
+        </div>
       </div>
     </>
   );
