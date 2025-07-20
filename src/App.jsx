35c3fe156e783b195ceff601041941dd8a43de2b
akonar-dev <157737602+akonar-dev@users.noreply.github.com>
2025-07-20 22:33:22 +0530

    Hero text animation and get started button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import fitfluencer from "../src/assets/tab-icon.png";
+import Typewriter from "typewriter-effect";
 
 function App() {
   return (
@@ -12,9 +13,19 @@ function App() {
               <li className="p-5 m-5 text-6xl">Contact Us</li>
             </ul>
         </div>
-        <div className="font-poppins text-9xl text-center text-white tracking-wider relative top-50">
-          Your Fitness Journey Starts Here.
+        <div className="font-poppins text-9xl text-center text-white tracking-wider relative top-50 font-extrabold uppercase drop-shadow-lg
+        ">
+          <Typewriter
+          options={{
+            strings: ['Your Fitness Journey Begins Here.'],
+            autoStart: true,
+            loop: true,
+            delay: 75,
+            deleteSpeed: 50,
+          }}
+        />
         </div>
+        <button type="button" class="py-10 px-10 me-2 mb-2 text-8xl text-poppins font-medium text-gray-900 focus:outline-none bg-white rounded-2xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:white-100 dark:focus:ring-black-100 dark:bg-gray-100 dark:text-white-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 absolute top-250 left-650">Get Started ➤</button>
       </div>
     </>
   );
